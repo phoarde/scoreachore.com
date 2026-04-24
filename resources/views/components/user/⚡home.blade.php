03675f8a85fed5bb7d4a5de8ba08bda9ceac512e
phoarde <fordinpate@gmail.com>
2026-04-24 01:47:34 -0500

    more mobil user dashboarc fixes

--- resources/views/components/user/⚡home.blade.php
+++ resources/views/components/user/⚡home.blade.php
@@ -9,25 +9,23 @@ new class extends component
 // change
 }
 ?>
-<div class="grid h-screen w-autn auto-rows-max  md:flex  flex-1 grid-cols-1 position-relative aspect overflow-hidden rounded-xl">
-    <flux:icon.wrench-screwdriver class=" text-4xl text-balance ">Welcome to Scorerachore.com  </flux:icon.wrench-screwdriver>
-                        <flux.callout class="flex flex-1 w-auto col-1 text-red-500 text-xl text-left ">Please bear with us we revitalize our user interface.</flux.callout>
-                    <flux:card class=" flex flex-1 max-w-1/2  text-gray-200 text-balance"> Soon booking from Apple IOS and Android app will make scheduling and communicating immeasurably more intuitive, real time tracking and video as our team members complete the assigned service.  We well send you an email when its up and 100% functional.This site will be deployed several times as day while the UI is updated feel free to comment on our progress and leave or if there is more you would like to see. . If you have questions or concerns. <class=" text-blue-400 <a  " href="mailto:info@scoreachore.com">
-                                                please email info@scorerachore.com
-                                                </a>
+<div class="flex flex-1 h-screen w-auto auto-rows-max  md:flex-flex-1 flex-cols-1 position-absolute aspect-video overflow-visable rounded-xl">
+    <flux:icon.wrench-screwdriver variant="solid"  class="position-relative  text-shadow-white text-4xl text-lg-center ">Welcome to Scorerachore.com  </flux:icon.wrench-screwdriver>
+        <flux:callout class="flex flex-1 w-auto flex-col col-1 text-red-300">Please bear with us we revitalize our user interface.<flux:callout>
+                    <flux:card class=" flex-column w-1/3  text-gray-200  text-sm text balance"> Soon booking from Apple IOS and Android app will make scheduling and communicating immeasurably more intuitive, real time tracking and video as our team members complete the assigned service.  We well send you an email when its up and 100% functional.This site will be deployed several times as day while the UI is updated feel free to comment on our progress and leave or if there is more you would like to see. . If you have questions or concerns. <class=" text-blue-200 <a  href="mailto:info@scoreachore.com">please email info@scorerachore.com</a>
                     </flux:card>
-
-
+        </flux:callout>
 
                 <div class="flex col-1 h-fit w-aoto position-relative aspect-video rounded-xl">
                     <div class="border-white border-opacity-50 ">
                         <livewire:service.service-table/>
                         <livewire:service.service-order/>
                     </div>
-                </div>
-
 </div>
 
 
 
 
+
+
+</div>
